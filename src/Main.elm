89c52d7f0febdaf1ba164b1e6eadf8305745e673
schokotets <schokotets@gmail.com>
2021-02-18 14:32:42 +0100

    source code formatting

--- src/Main.elm
+++ src/Main.elm
@@ -1,232 +1,298 @@
 -- Blackout page to draw & hide black boxes on an image
 
+
 module Main exposing (..)
 
 import Browser
 import Browser.Navigation as Nav
+import Html exposing (Attribute, Html, a, br, button, div, h1, h2, img, map, text)
+import Html.Attributes exposing (href, src, style)
+import Html.Events exposing (on, onClick)
+import Http
+import Json.Decode as Decode
+import Json.Encode as Encode
 import Url exposing (Url)
 import Url.Parser exposing ((<?>), query, s)
 import Url.Parser.Query
-import Html exposing (Html, Attribute, button, div, text, img, map, h1, h2, a, br)
-import Html.Events exposing (onClick, on)
-import Html.Attributes exposing (src, style, href)
-import Json.Decode as Decode
-import Json.Encode as Encode
-import Http
+
+
 
 -- MAIN
 
+
 main =
-  Browser.application
-    { init = init
-    , subscriptions = subscriptions
-    , update = update
-    , view = view
-    , onUrlChange = UrlChanged
-    , onUrlRequest = LinkClicked
-    }
+    Browser.application
+        { init = init
+        , subscriptions = subscriptions
+        , update = update
+        , view = view
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
+
+
 
 -- MODEL
 
-type alias Box = 
-    { id: Int
-    , top: Float
-    , left: Float
-    , width: Float
-    , height: Float
-    , shown: Bool
+
+type alias Box =
+    { id : Int
+    , top : Float
+    , left : Float
+    , width : Float
+    , height : Float
+    , shown : Bool
     }
 
+
 type alias Position =
-    { x: Float, y: Float }
+    { x : Float, y : Float }
+
 
 type Mode
     = Viewing
     | Editing
     | Deleting
 
+
 type alias DocumentInfo =
-    { boxes: List Box
-    , name: String
-    , prev: String
-    , next: String
-    , url: String
+    { boxes : List Box
+    , name : String
+    , prev : String
+    , next : String
+    , url : String
     }
 
 
-type alias Model = 
-    { document: String
-    , navkey: Nav.Key
-    , selecting: Bool
-    , documentinfo: DocumentInfo
-    , startpos: Position
-    , mode: Mode
+type alias Model =
+    { document : String
+    , navkey : Nav.Key
+    , selecting : Bool
+    , documentinfo : DocumentInfo
+    , startpos : Position
+    , mode : Mode
     }
+
 
 queryParser : Url.Parser.Parser (Maybe String -> Maybe String) (Maybe String)
 queryParser =
     s "index.html" <?> Url.Parser.Query.string "doc"
 
-init : () -> Url -> Nav.Key -> (Model, Cmd Msg)
+
+init : () -> Url -> Nav.Key -> ( Model, Cmd Msg )
 init _ url key =
-  let doc = Maybe.withDefault "0000" ( Maybe.withDefault Nothing (Url.Parser.parse queryParser url))
-  in (
-  { document = doc
-  , navkey = key
-  , selecting = False
-  , startpos = { x = 0.0, y = 0.0 }
-  , documentinfo =
-    { name = "Loading..."
-    , boxes = []
-    , prev = ""
-    , next = ""
-    , url = ""
-    }
-  , mode = Viewing
-  },
-  Http.get
-    { url = "/document?doc=" ++ doc
-    , expect = Http.expectJson GotData documentDecoder
-    }
-  )
+    let
+        doc =
+            Maybe.withDefault "0000" (Maybe.withDefault Nothing (Url.Parser.parse queryParser url))
+    in
+    ( { document = doc
+      , navkey = key
+      , selecting = False
+      , startpos = { x = 0.0, y = 0.0 }
+      , documentinfo =
+            { name = "Loading..."
+            , boxes = []
+            , prev = ""
+            , next = ""
+            , url = ""
+            }
+      , mode = Viewing
+      }
+    , Http.get
+        { url = "/document?doc=" ++ doc
+        , expect = Http.expectJson GotData documentDecoder
+        }
+    )
+
 
 documentDecoder : Decode.Decoder DocumentInfo
 documentDecoder =
-  Decode.map5 DocumentInfo
-    (Decode.field "boxes" (Decode.list boxDecoder))
-    (Decode.field "name" Decode.string)
-    (Decode.field "prev" Decode.string)
-    (Decode.field "next" Decode.string)
-    (Decode.field "url" Decode.string)
+    Decode.map5 DocumentInfo
+        (Decode.field "boxes" (Decode.list boxDecoder))
+        (Decode.field "name" Decode.string)
+        (Decode.field "prev" Decode.string)
+        (Decode.field "next" Decode.string)
+        (Decode.field "url" Decode.string)
+
 
 boxDecoder : Decode.Decoder Box
 boxDecoder =
-  Decode.map6 Box
-    (Decode.field "id" Decode.int)
-    (Decode.field "top" Decode.float)
-    (Decode.field "left" Decode.float)
-    (Decode.field "width" Decode.float)
-    (Decode.field "height" Decode.float)
-    (Decode.succeed True) -- shown
+    Decode.map6 Box
+        (Decode.field "id" Decode.int)
+        (Decode.field "top" Decode.float)
+        (Decode.field "left" Decode.float)
+        (Decode.field "width" Decode.float)
+        (Decode.field "height" Decode.float)
+        (Decode.succeed True)
+
+
+
+-- shown
 
 
 encodeDocument : Model -> Encode.Value
 encodeDocument model =
-  Encode.object
-    [ ( "boxes", Encode.list (\box ->
-        Encode.object
-          [ ("id", Encode.int box.id),
-            ("top", Encode.float box.top),
-            ("left", Encode.float box.left),
-            ("width", Encode.float box.width),
-            ("height", Encode.float box.height)
-          ]
-      ) model.documentinfo.boxes
-      )
-    ]
+    Encode.object
+        [ ( "boxes"
+          , Encode.list
+                (\box ->
+                    Encode.object
+                        [ ( "id", Encode.int box.id )
+                        , ( "top", Encode.float box.top )
+                        , ( "left", Encode.float box.left )
+                        , ( "width", Encode.float box.width )
+                        , ( "height", Encode.float box.height )
+                        ]
+                )
+                model.documentinfo.boxes
+          )
+        ]
+
 
 
 -- UPDATE
 
 
 type Msg
-  = Toggle Int
-  | ClickPosition Position
-  | GotData (Result Http.Error DocumentInfo)
-  | GotText (Result Http.Error String)
-  | Save
-  | LinkClicked Browser.UrlRequest
-  | UrlChanged Url
-  | SetModeViewing
-  | SetModeEditing
+    = Toggle Int
+    | ClickPosition Position
+    | GotData (Result Http.Error DocumentInfo)
+    | GotText (Result Http.Error String)
+    | Save
+    | LinkClicked Browser.UrlRequest
+    | UrlChanged Url
+    | SetModeViewing
+    | SetModeEditing
+
 
 min : Float -> Float -> Float
-min a b = if a < b then a else b
+min a b =
+    if a < b then
+        a
 
-update : Msg -> Model -> (Model, Cmd Msg)
+    else
+        b
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Toggle id ->
-      let documentinfo = model.documentinfo
-      in ( { model |
-          documentinfo =
-            { documentinfo
-            | boxes = List.map (\box ->
-              if box.id == id then
-                { box | shown = not box.shown}
-              else box
-            ) documentinfo.boxes }
-      }, Cmd.none)
+    case msg of
+        Toggle id ->
+            let
+                documentinfo =
+                    model.documentinfo
+            in
+            ( { model
+                | documentinfo =
+                    { documentinfo
+                        | boxes =
+                            List.map
+                                (\box ->
+                                    if box.id == id then
+                                        { box | shown = not box.shown }
 
-    ClickPosition pos ->
-      if not model.selecting then
-        ( { model |
-            startpos = pos,
-            selecting = True }
-        , Cmd.none )
-      else
-        let documentinfo = model.documentinfo
-        in ( { model |
-          selecting = False,
-          documentinfo =
-            { documentinfo
-            | boxes =
-               [ { id = List.length documentinfo.boxes,
-                   left = 100 * min pos.x model.startpos.x,
-                   top  = 100 * min pos.y model.startpos.y,
-                   width  = 100 * abs(pos.x-model.startpos.x),
-                   height = 100 * abs(pos.y-model.startpos.y),
-                   shown = True} ]
-               ++ documentinfo.boxes
-            }
-        }, Cmd.none)
-    GotText _ ->
-      (model, Cmd.none)
-    GotData result ->
-      case result of
-        Ok documentinfo ->
-          ( { model
-            | documentinfo = documentinfo }
-          , Cmd.none )
-            --Debug.log fullText (model, Cmd.none)
-        Err _ ->
-          (model, Cmd.none)
-    Save ->
-      (model, Http.post
-        { url = "/document?doc=" ++ model.document
-        , expect = Http.expectString GotText
-        , body = Http.jsonBody ( encodeDocument model )
-        }
-      )
+                                    else
+                                        box
+                                )
+                                documentinfo.boxes
+                    }
+              }
+            , Cmd.none
+            )
 
-    LinkClicked urlRequest ->
-      case urlRequest of
-        Browser.Internal url ->
-          ( model, Nav.load (Url.toString url) )
-          --( model, Nav.pushUrl model.key (Url.toString url) )
+        ClickPosition pos ->
+            if not model.selecting then
+                ( { model
+                    | startpos = pos
+                    , selecting = True
+                  }
+                , Cmd.none
+                )
 
-        Browser.External href ->
-          ( model, Nav.load href )
+            else
+                let
+                    documentinfo =
+                        model.documentinfo
+                in
+                ( { model
+                    | selecting = False
+                    , documentinfo =
+                        { documentinfo
+                            | boxes =
+                                [ { id = List.length documentinfo.boxes
+                                  , left = 100 * min pos.x model.startpos.x
+                                  , top = 100 * min pos.y model.startpos.y
+                                  , width = 100 * abs (pos.x - model.startpos.x)
+                                  , height = 100 * abs (pos.y - model.startpos.y)
+                                  , shown = True
+                                  }
+                                ]
+                                    ++ documentinfo.boxes
+                        }
+                  }
+                , Cmd.none
+                )
 
-    UrlChanged url ->
-      ( model, Cmd.none )
+        GotText _ ->
+            ( model, Cmd.none )
 
-    SetModeEditing ->
-      ( { model
-        | mode = Editing }
-      , Cmd.none )
+        GotData result ->
+            case result of
+                Ok documentinfo ->
+                    ( { model
+                        | documentinfo = documentinfo
+                      }
+                    , Cmd.none
+                    )
 
-    SetModeViewing ->
-      ( { model
-        | mode = Viewing }
-      , Cmd.none )
+                --Debug.log fullText (model, Cmd.none)
+                Err _ ->
+                    ( model, Cmd.none )
+
+        Save ->
+            ( model
+            , Http.post
+                { url = "/document?doc=" ++ model.document
+                , expect = Http.expectString GotText
+                , body = Http.jsonBody (encodeDocument model)
+                }
+            )
+
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.load (Url.toString url) )
+
+                --( model, Nav.pushUrl model.key (Url.toString url) )
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( model, Cmd.none )
+
+        SetModeEditing ->
+            ( { model
+                | mode = Editing
+              }
+            , Cmd.none
+            )
+
+        SetModeViewing ->
+            ( { model
+                | mode = Viewing
+              }
+            , Cmd.none
+            )
+
 
 
 -- SUBSCRIPTIONS
 
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
+
 
 
 -- VIEW
@@ -234,81 +300,131 @@ subscriptions model =
 
 view : Model -> Browser.Document Msg
 view model =
-  { title = ""
-  , body = [ div [] [
-    div []
-    [ h1 [ style "display" "inline-block", style "margin" "0 0.5rem" ] [ text "Blackout" ]
-    , h2 [ style "display" "inline-block", style "margin" "0 0.5rem" ] [ text model.documentinfo.name ]
-    , br [] []
-    , a
-      [ href ("/index.html?doc=" ++ model.documentinfo.prev)
-      , style "margin" "0.5em" ] [ text "Prev" ]
-    , a
-      [ href "/menu.html"
-      , style "margin" "0.5em" ] [ text "Menu" ]
-    , a
-      [ href ("/index.html?doc=" ++ model.documentinfo.next)
-      , style "margin" "0.5em" ] [ text "Next" ]
-    , button [ onClick (if model.mode == Editing then SetModeViewing else SetModeEditing) ]
-      [ text (if model.mode == Editing then "Done" else "Edit") ]
-    , button [ onClick Save ] [ text "Save" ]
-    ]
-    , div [ style "position" "relative" ] [
-        img [
-          style "width" "100%",
-          style "filter" (if model.selecting then "brightness(0.9)" else "unset"),
-          src model.documentinfo.url
-        ] [ text "+" ],
-        blackBoxes model
-      ]
-    ]
-    ]}
+    { title = ""
+    , body =
+        [ div []
+            [ div []
+                [ h1 [ style "display" "inline-block", style "margin" "0 0.5rem" ] [ text "Blackout" ]
+                , h2 [ style "display" "inline-block", style "margin" "0 0.5rem" ] [ text model.documentinfo.name ]
+                , br [] []
+                , a
+                    [ href ("/index.html?doc=" ++ model.documentinfo.prev)
+                    , style "margin" "0.5em"
+                    ]
+                    [ text "Prev" ]
+                , a
+                    [ href "/menu.html"
+                    , style "margin" "0.5em"
+                    ]
+                    [ text "Menu" ]
+                , a
+                    [ href ("/index.html?doc=" ++ model.documentinfo.next)
+                    , style "margin" "0.5em"
+                    ]
+                    [ text "Next" ]
+                , button
+                    [ onClick
+                        (if model.mode == Editing then
+                            SetModeViewing
+
+                         else
+                            SetModeEditing
+                        )
+                    ]
+                    [ text
+                        (if model.mode == Editing then
+                            "Done"
+
+                         else
+                            "Edit"
+                        )
+                    ]
+                , button [ onClick Save ] [ text "Save" ]
+                ]
+            , div [ style "position" "relative" ]
+                [ img
+                    [ style "width" "100%"
+                    , style "filter"
+                        (if model.selecting then
+                            "brightness(0.9)"
+
+                         else
+                            "unset"
+                        )
+                    , src model.documentinfo.url
+                    ]
+                    [ text "+" ]
+                , blackBoxes model
+                ]
+            ]
+        ]
+    }
+
 
 blackBoxes : Model -> Html Msg
-blackBoxes model = 
-  div [] (
-      -- A div to capture the non-box click events
-      div ( List.append
-        [ style "position" "absolute"
-        , style "width" "100%"
-        , style "height" "100%"
-        , style "top" "0px"
-        , style "left" "0px"
-        ] (
-          if model.mode == Editing
-          then [
-            style "cursor" "crosshair",
-            onClickCreateBox
-          ] else []
-        ))
-      [] :: (
-      List.map (\box -> div
-        [ style "position" "absolute"
-        , style "backgroundColor" ( if box.shown then "black" else "unset" )
-        , style "top" ( String.fromFloat ( box.top - 0.5 ) ++ "%")
-        , style "left" ( String.fromFloat ( box.left - 0.5 ) ++ "%")
-        , style "width" ( String.fromFloat ( box.width + 1 ) ++ "%")
-        , style "height" ( String.fromFloat ( box.height + 1 ) ++ "%")
-        , style "cursor" "pointer"
-        , onClick (Toggle box.id)
-        ] []) model.documentinfo.boxes
-    ) )
+blackBoxes model =
+    div []
+        -- A div to capture the non-box click events
+        (div
+            (List.append
+                [ style "position" "absolute"
+                , style "width" "100%"
+                , style "height" "100%"
+                , style "top" "0px"
+                , style "left" "0px"
+                ]
+                (if model.mode == Editing then
+                    [ style "cursor" "crosshair"
+                    , onClickCreateBox
+                    ]
+
+                 else
+                    []
+                )
+            )
+            []
+            :: List.map
+                (\box ->
+                    div
+                        [ style "position" "absolute"
+                        , style "backgroundColor"
+                            (if box.shown then
+                                "black"
+
+                             else
+                                "unset"
+                            )
+                        , style "top" (String.fromFloat (box.top - 0.5) ++ "%")
+                        , style "left" (String.fromFloat (box.left - 0.5) ++ "%")
+                        , style "width" (String.fromFloat (box.width + 1) ++ "%")
+                        , style "height" (String.fromFloat (box.height + 1) ++ "%")
+                        , style "cursor" "pointer"
+                        , onClick (Toggle box.id)
+                        ]
+                        []
+                )
+                model.documentinfo.boxes
+        )
+
 
 genPosition : Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> Msg
 genPosition sleft stop left top w h x y =
-    ClickPosition ({
-        x = toFloat (x-left+sleft)/(toFloat w),
-        y = toFloat (y-top +stop )/(toFloat h)
-    })
+    ClickPosition
+        { x = toFloat (x - left + sleft) / toFloat w
+        , y = toFloat (y - top + stop) / toFloat h
+        }
+
 
 onClickCreateBox : Attribute Msg
 onClickCreateBox =
-  on "click" (Decode.map8 genPosition
-      (Decode.at [ "target", "parentNode", "parentNode", "parentNode", "parentNode", "parentNode", "scrollLeft" ] Decode.int)
-      (Decode.at [ "target", "parentNode", "parentNode", "parentNode", "parentNode", "parentNode", "scrollTop" ] Decode.int)
-      (Decode.at [ "target", "parentNode", "parentNode", "offsetLeft" ] Decode.int)
-      (Decode.at [ "target", "parentNode", "parentNode", "offsetTop" ] Decode.int)
-      (Decode.at [ "target", "offsetWidth" ] Decode.int)
-      (Decode.at [ "target", "offsetHeight" ] Decode.int)
-      (Decode.at [ "clientX" ] Decode.int)
-      (Decode.at [ "clientY" ] Decode.int))
+    on "click"
+        (Decode.map8 genPosition
+            (Decode.at [ "target", "parentNode", "parentNode", "parentNode", "parentNode", "parentNode", "scrollLeft" ] Decode.int)
+            (Decode.at [ "target", "parentNode", "parentNode", "parentNode", "parentNode", "parentNode", "scrollTop" ] Decode.int)
+            (Decode.at [ "target", "parentNode", "parentNode", "offsetLeft" ] Decode.int)
+            (Decode.at [ "target", "parentNode", "parentNode", "offsetTop" ] Decode.int)
+            (Decode.at [ "target", "offsetWidth" ] Decode.int)
+            (Decode.at [ "target", "offsetHeight" ] Decode.int)
+            (Decode.at [ "clientX" ] Decode.int)
+            (Decode.at [ "clientY" ] Decode.int)
+        )
--- src/Menu.elm
+++ src/Menu.elm
@@ -1,87 +1,109 @@
 -- Menu to select different documents
 
+
 module Menu exposing (..)
 
 import Browser
 import Browser.Navigation as Nav
-import Html exposing (Html, Attribute, button, div, text, img, map, h1, h2, ul, li)
-import Html.Events exposing (onClick, on)
+import Html exposing (Attribute, Html, button, div, h1, h2, img, li, map, text, ul)
 import Html.Attributes exposing (src, style)
+import Html.Events exposing (on, onClick)
+import Http
 import Json.Decode as Decode
 import Json.Encode as Encode
-import Http
+
+
 
 -- MAIN
 
+
 main =
-  Browser.element {
-    init = init,
-    subscriptions = subscriptions,
-    update = update,
-    view = view }
+    Browser.element
+        { init = init
+        , subscriptions = subscriptions
+        , update = update
+        , view = view
+        }
+
+
 
 -- MODEL
 
-type alias Document = 
-    { id: String
-    , url: String
-    , name: String
-    }
 
-type alias Model = 
-    { documents: List Document
+type alias Document =
+    { id : String
+    , url : String
+    , name : String
     }
 
 
-init : () -> (Model, Cmd Msg)
-init _ = (
-  { documents = []
-  },
-  Http.get
-    { url = "/documentlist"
-    , expect = Http.expectJson GotData documentListDecoder
+type alias Model =
+    { documents : List Document
     }
-  )
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { documents = []
+      }
+    , Http.get
+        { url = "/documentlist"
+        , expect = Http.expectJson GotData documentListDecoder
+        }
+    )
 
 
 documentListDecoder : Decode.Decoder (List Document)
 documentListDecoder =
-  Decode.field "documents" (Decode.list documentDecoder)
+    Decode.field "documents" (Decode.list documentDecoder)
+
 
 documentDecoder : Decode.Decoder Document
 documentDecoder =
-  Decode.map3 Document
-    (Decode.field "id" Decode.string)
-    (Decode.field "url" Decode.string)
-    (Decode.field "name" Decode.string)
+    Decode.map3 Document
+        (Decode.field "id" Decode.string)
+        (Decode.field "url" Decode.string)
+        (Decode.field "name" Decode.string)
+
+
 
 -- UPDATE
 
-type Msg
-  = Navigate String
-  | GotData (Result Http.Error (List Document))
 
-update : Msg -> Model -> (Model, Cmd Msg)
+type Msg
+    = Navigate String
+    | GotData (Result Http.Error (List Document))
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Navigate doc ->
-      ( model
-      , Nav.load ("/index.html?doc="++doc) )
-    GotData result ->
-      case result of
-        Ok documents ->
-          ( { model |
-              documents = documents }
-          , Cmd.none )
-        Err _ ->
-          (model, Cmd.none)
+    case msg of
+        Navigate doc ->
+            ( model
+            , Nav.load ("/index.html?doc=" ++ doc)
+            )
+
+        GotData result ->
+            case result of
+                Ok documents ->
+                    ( { model
+                        | documents = documents
+                      }
+                    , Cmd.none
+                    )
+
+                Err _ ->
+                    ( model, Cmd.none )
+
 
 
 -- SUBSCRIPTIONS
 
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
+
 
 
 -- VIEW
@@ -89,16 +111,22 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ h1 [] [ text "Blackout" ]
-    , h2 [] [ text "Menü" ]
-    , ul [] (documentList model.documents)
-    ]
+    div []
+        [ h1 [] [ text "Blackout" ]
+        , h2 [] [ text "Menü" ]
+        , ul [] (documentList model.documents)
+        ]
+
 
 documentList : List Document -> List (Html Msg)
-documentList documents = 
-  List.map (\document -> li
-    [ style "cursor" "pointer"
-    , style "margin" "1em 0"
-    , onClick (Navigate document.id)
-    ] [ text document.name ]) documents
+documentList documents =
+    List.map
+        (\document ->
+            li
+                [ style "cursor" "pointer"
+                , style "margin" "1em 0"
+                , onClick (Navigate document.id)
+                ]
+                [ text document.name ]
+        )
+        documents
